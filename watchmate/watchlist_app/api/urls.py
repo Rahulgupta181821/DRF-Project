214095ff8d2d77638431151dbb1562dbe2d42e04
Rahul Kumar Gupta <kumarrahul18699@gmail.com>
2026-01-26 19:16:06 +0530

    [REM]extra code

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from watchlist_app.api.views import *
 urlpatterns = [
-    # path('list/',movie_list,name='movie-list'),
-    # path('<int:pk>',movie_detail,name='movie-detail'),
     path('list/',WatchListAV.as_view(),name='movie-list'),
     path('<int:pk>',WatchDetailAV.as_view(),name='movie-detail'),
     path('stream/',StreamPlateformAV.as_view(),name='stream-list'),
